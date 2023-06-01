8fc5e69372e9c88abb90dca44416b817b26ee930
Olaf Mihari <olafnjenga@gmail.com>
2023-06-01 03:50:02 +0300

    completed assignment

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,11 @@
 class Network < ActiveRecord::Base
-  
+    has_many :shows
+
+    def shows_list
+      self.shows.map { |show| show.name }
+    end
+
+    def sorry
+      "We're sorry about passing on John Mulaney's pilot"
+    end
 end
